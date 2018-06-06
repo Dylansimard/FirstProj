734e5536815e47d0cf8dc05526f2a51d6de52e6b
dsimard <dsimard@SupportCenter4.rd.unr.edu>
2018-06-06 15:11:40 -0700

    WebCampus login page fix complete? only private and reset but that's kinda all?

--- GUILearning/src/testing/WebCampusLogin.java
+++ GUILearning/src/testing/WebCampusLogin.java
@@ -11,6 +11,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class WebCampusLogin {
 	
@@ -50,7 +54,7 @@ public class WebCampusLogin {
 		lblNewLabel.setBounds(10, 11, 766, 50);
 		frame.getContentPane().add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("1.) Verify user has an active netID in the security tool");
+		JLabel lblNewLabel_1 = new JLabel("1.) Verify user has an active NetID in the security tool");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_1.setBounds(20, 72, 331, 32);
 		frame.getContentPane().add(lblNewLabel_1);
@@ -98,7 +102,6 @@ public class WebCampusLogin {
 				if(isMacChecked) {
 					isMacChecked = false;
 					chckbxMac.setSelected(false);
-					System.out.println("test");
 				}
 			}
 		});
@@ -140,6 +143,9 @@ public class WebCampusLogin {
 			public void actionPerformed(ActionEvent e) {
 				if(isSafariChecked) {
 					isSafariChecked = false;
+				}
+				else if(isWindowsChecked) {
+					chckbxSafari.setSelected(false);
 				}
 				else {
 					isSafariChecked = true;
@@ -224,6 +230,122 @@ public class WebCampusLogin {
 		chckbxFirefox.setBounds(96, 366, 114, 37);
 		frame.getContentPane().add(chckbxFirefox);
 		
+		
+		JLabel lblMacFirefox = new JLabel("Cmd-Shift-p");
+		JLabel lblMacChrome = new JLabel("Cmd-Shift-n");
+		JLabel lblMacSafari = new JLabel("Cmd-Shift-n");
+		JLabel labelWindowsFirefox = new JLabel("Ctrl-Shift-p");
+		JLabel lblWindowsChrome = new JLabel("Ctrl-Shift-n");
+		JLabel lblWrong = new JLabel("Please select two valid options");
+		
+		JButton btnContinue = new JButton("Continue");
+		btnContinue.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(isWindowsChecked && isChromeChecked) {
+					lblWindowsChrome.setVisible(true);
+					lblWrong.setVisible(false);
+					lblMacFirefox.setVisible(false);
+					lblMacChrome.setVisible(false);
+					lblMacSafari.setVisible(false);
+					labelWindowsFirefox.setVisible(false);
+				}
+				else if(isWindowsChecked && isFirefoxChecked) {
+					labelWindowsFirefox.setVisible(true);
+					lblWrong.setVisible(false);
+					lblMacFirefox.setVisible(false);
+					lblMacChrome.setVisible(false);
+					lblMacSafari.setVisible(false);
+					lblWindowsChrome.setVisible(false);
+				}
+				else if(isMacChecked && isSafariChecked) {
+					lblMacSafari.setVisible(true);
+					lblWrong.setVisible(false);
+					lblMacFirefox.setVisible(false);
+					lblMacChrome.setVisible(false);
+					labelWindowsFirefox.setVisible(false);
+					lblWindowsChrome.setVisible(false);
+				}
+				else if(isMacChecked && isChromeChecked) {
+					lblMacChrome.setVisible(true);
+					lblWrong.setVisible(false);
+					lblMacFirefox.setVisible(false);
+					lblMacSafari.setVisible(false);
+					labelWindowsFirefox.setVisible(false);
+					lblWindowsChrome.setVisible(false);
+				}
+				else if(isMacChecked && isFirefoxChecked) {
+					lblMacFirefox.setVisible(true);
+					lblWrong.setVisible(false);
+					lblMacChrome.setVisible(false);
+					lblMacSafari.setVisible(false);
+					labelWindowsFirefox.setVisible(false);
+					lblWindowsChrome.setVisible(false);
+				}
+				else {
+					lblWrong.setVisible(true);
+					lblMacFirefox.setVisible(false);
+					lblMacChrome.setVisible(false);
+					lblMacSafari.setVisible(false);
+					labelWindowsFirefox.setVisible(false);
+					lblWindowsChrome.setVisible(false);
+				}
+			}
+		});
+
+		btnContinue.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnContinue.setBounds(30, 432, 252, 57);
+		frame.getContentPane().add(btnContinue);
+		
+		
+		lblWrong.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblWrong.setBounds(410, 259, 350, 144);
+		frame.getContentPane().add(lblWrong);
+		lblWrong.setVisible(false);
+		
+		
+		lblMacFirefox.setFont(new Font("Tahoma", Font.PLAIN, 50));
+		lblMacFirefox.setBounds(459, 259, 269, 144);
+		frame.getContentPane().add(lblMacFirefox);
+		lblMacFirefox.setVisible(false);
+		
+		
+		lblMacChrome.setFont(new Font("Tahoma", Font.PLAIN, 50));
+		lblMacChrome.setBounds(482, 259, 271, 144);
+		frame.getContentPane().add(lblMacChrome);
+		lblMacChrome.setVisible(false);
+		
+		
+		lblMacSafari.setFont(new Font("Tahoma", Font.PLAIN, 50));
+		lblMacSafari.setBounds(447, 259, 274, 144);
+		frame.getContentPane().add(lblMacSafari);
+		lblMacSafari.setVisible(false);
+		
+		
+		labelWindowsFirefox.setFont(new Font("Tahoma", Font.PLAIN, 50));
+		labelWindowsFirefox.setBounds(460, 259, 260, 144);
+		frame.getContentPane().add(labelWindowsFirefox);
+		labelWindowsFirefox.setVisible(false);
+		
+		
+		lblWindowsChrome.setFont(new Font("Tahoma", Font.PLAIN, 50));
+		lblWindowsChrome.setBounds(450, 259, 260, 144);
+		frame.getContentPane().add(lblWindowsChrome);
+		
+		
+		
+		JButton btnNewButton = new JButton("Reset NetID Password\r\n");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				frame.dispose();
+				NetIDResetWindow netIDReset = new NetIDResetWindow();
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton.setBounds(399, 432, 354, 57);
+		frame.getContentPane().add(btnNewButton);
+		lblWindowsChrome.setVisible(false);
 
 	}
 }
